6ee3f12480b1e7bf9594b2f1f3dbf07f825ab1c4
João Pedro Leal Salvador Chiappa <joao.chiappa@pmu.net>
2023-09-04 13:54:02 -0300

    Deploy configs

--- tcc-rpg/src/index.js
+++ tcc-rpg/src/index.js
@@ -1,4 +1,11 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
