a04cb19b6adc39e3051f4b6957f2c8f24d874f17
João Pedro Leal Salvador Chiappa <joao.chiappa@pmu.net>
2023-09-15 12:37:47 -0300

    Style for chat

--- tcc-rpg/src/pages/chat/ChatRoom.js
+++ tcc-rpg/src/pages/chat/ChatRoom.js
@@ -10,6 +10,7 @@ import {
 } from "firebase/firestore";
 import { useHistory } from "react-router-dom";
 import { db, auth } from "../../firebaseConfig";
+import "../../css/Chat.css";
 
 function ChatRoom({ currentUserId, initialRoomId }) {
   const [roomId, setRoomId] = useState(
@@ -18,7 +19,19 @@ function ChatRoom({ currentUserId, initialRoomId }) {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [currentUserData, setCurrentUserData] = useState(null);
+  const [userColors, setUserColors] = useState({});
+  const [roomName, setRoomName] = useState("");
+
   const history = useHistory();
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
   const generateInviteLink = () => {
     return window.location.origin + "/tcc-react/join/" + roomId;
@@ -29,6 +42,7 @@ function ChatRoom({ currentUserId, initialRoomId }) {
       const roomRef = await addDoc(collection(db, "chatRooms"), {
         createdAt: new Date(),
         members: [currentUserId],
+        name: roomName,
       });
       setRoomId(roomRef.id);
       localStorage.setItem("roomId", roomRef.id);
@@ -66,6 +80,11 @@ function ChatRoom({ currentUserId, initialRoomId }) {
       setRoomId(localStorage.getItem("roomId"));
     }
 
+    const savedUserColors = localStorage.getItem("userColors");
+    if (savedUserColors) {
+      setUserColors(JSON.parse(savedUserColors));
+    }
+
     const userLogin = auth.onAuthStateChanged(async (user) => {
       if (user) {
         const userRef = doc(db, "users", user.uid);
@@ -82,7 +101,14 @@ function ChatRoom({ currentUserId, initialRoomId }) {
       const messagesRef = collection(db, "chatRooms", roomId, "messages");
       const q = query(messagesRef, orderBy("timestamp", "desc"));
 
-      // Ouvir as alterações nas mensagens em tempo real
+      const roomRef = doc(db, "chatRooms", roomId);
+      const fetchRoomData = async () => {
+        const roomSnapshot = await getDoc(roomRef);
+        const roomData = roomSnapshot.data();
+        setRoomName(roomData.name);
+      };
+      fetchRoomData();
+
       const unsubscribe = onSnapshot(q, (snapshot) => {
         const fetchedMessages = snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -96,12 +122,18 @@ function ChatRoom({ currentUserId, initialRoomId }) {
   }, [roomId]);
 
   return (
-    <div>
+    <div className="chat-box">
+      <input
+        type="text"
+        value={roomName}
+        onChange={(e) => setRoomName(e.target.value)}
+        placeholder="Nome da Sala"
+      />
       <button onClick={createRoom}>Criar Sala</button>
 
       {roomId && (
         <div>
-          <h2>Sala de Chat: {roomId}</h2>
+          <h2>Sala de Chat: {roomName}</h2>
 
           <button
             onClick={() => navigator.clipboard.writeText(generateInviteLink())}
@@ -110,11 +142,27 @@ function ChatRoom({ currentUserId, initialRoomId }) {
           </button>
 
           <div className="chat-messages">
-            {messages.map((msg) => (
-              <div key={msg.id}>
-                <strong>{msg.username}: </strong> {msg.content}
-              </div>
-            ))}
+            {messages.map((msg) => {
+              let userColor = userColors[msg.username];
+              if (!userColor) {
+                userColor = getRandomColor();
+                const newUserColors = {
+                  ...userColors,
+                  [msg.username]: userColor,
+                };
+                setUserColors(newUserColors);
+                localStorage.setItem(
+                  "userColors",
+                  JSON.stringify(newUserColors)
+                );
+              }
+              return (
+                <div key={msg.id} className="message">
+                  <strong style={{ color: userColor }}>{msg.username}: </strong>
+                  <span className="message-content">{msg.content}</span>
+                </div>
+              );
+            })}
           </div>
 
           <input
